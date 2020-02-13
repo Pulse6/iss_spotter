e12b67e2dfca80199259fd0dff091d6f0979a1a5
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-13 23:43:54 +0000

    can get latitude and lonitude

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 });
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('162.245.144.188', (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned Coords:' , coords);
+});
--- iss.js
+++ iss.js
@@ -28,7 +28,26 @@ const fetchMyIP = function(callback) {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+  request(`https://ipvigilante.com/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching Coordinates for IP: ${body}`), null);
+      return;
+    }
+    const latitude = JSON.parse(body).data.latitude;
+    const longitude = JSON.parse(body).data.longitude;
+    // const { latitude, longitude } = JSON.parse(body).data;
+
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
 
 // const ip = JSON.parse(body).ip;
 // callback(null, ip);
