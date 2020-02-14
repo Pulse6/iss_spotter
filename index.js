3aefa86c446cef3aa479088fa5e4359d509e0978
Hok Lun Siu (Marco) <marco698698@hotmail.com>
2020-02-14 02:21:06 +0000

    updating index.js, index2.js, iss_promised.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ fetchMyIP((error, ip) => {
 
 const { fetchCoordsByIP } = require('./iss');
 
-fetchCoordsByIP('162.245.144.188', (error, coords) => {
+fetchCoordsByIP('66.207.199.230', (error, coords) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
@@ -41,10 +41,19 @@ fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
 
 const { nextISSTimesForMyLocation } = require('./iss');
 
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
 nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
     return console.log("It didn't work!", error);
   }
   // success, print out the deets!
-  console.log(passTimes);
+  printPassTimes(passTimes);
 });
